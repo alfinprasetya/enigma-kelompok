fc2dfd19c6d7f18c36c259e556fe7657e20fddf6
Alfin Ade Prasetya <alfin.prasetya24@gmail.com>
2024-07-05 18:50:39 +0700

    initial commit

---
+++ src/main/java/com/enigma/kelompok/KelompokApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.kelompok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelompokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelompokApplication.class, args);
+	}
+
+}
