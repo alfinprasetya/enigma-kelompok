ee7dd055e6ffa7938a8736a27bd0accaa74764c9
ikram <ikrammarbasy17@gmail.com>
2024-07-06 12:18:12 +0700

    Update for stock, add specification and controller

---
+++ src/main/java/com/enigma/kelompok/controller/StockController.java
@@ -0,0 +1,55 @@
+package com.enigma.kelompok.controller;
+
+import com.enigma.kelompok.model.Stock;
+import com.enigma.kelompok.service.StockService;
+import com.enigma.kelompok.utils.response.PageWrapper;
+import com.enigma.kelompok.utils.response.Res;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/stocks")
+@RequiredArgsConstructor
+public class StockController {
+
+    private final StockService stockService;
+
+    @GetMapping
+    public ResponseEntity<?> getAll(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String code,
+            @RequestParam(required = false) Integer price,
+            @PageableDefault(page = 0, size = 10)Pageable pageable) {
+        ResponseEntity<?> response = (ResponseEntity<?>) stockService.getAll(name, code, price);
+        return response;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOne(@PathVariable Integer id) {
+        Stock stock = stockService.getOne(id);
+        return Res.renderJson(stock, "Success", HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody Stock req) {
+        Stock createdStock = stockService.create(req);
+        return Res.renderJson(createdStock, "Stock created successfully", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody Stock req) {
+        Stock updatedStock = stockService.update(id, req);
+        return Res.renderJson(updatedStock, "Stock updated successfully", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id){
+        stockService.delete(id);
+    }
+}
--- src/main/java/com/enigma/kelompok/service/StockService.java
+++ src/main/java/com/enigma/kelompok/service/StockService.java
@@ -5,7 +5,8 @@ import com.enigma.kelompok.model.Stock;
 import java.util.List;
 
 public interface StockService {
-    List<Stock> getAll();
+    //Bagian getAll
+    List<Stock> getAll(String name, String code, Integer price);
     Stock getOne(Integer id);
     Stock create(Stock req);
     Stock update(Integer id, Stock req);
