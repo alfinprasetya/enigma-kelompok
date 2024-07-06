58dbd11a6be412912f16fe0085eaca214f158acc
Alfin Ade Prasetya <alfin.prasetya24@gmail.com>
2024-07-06 10:32:41 +0700

    Fix: User repository

--- src/main/java/com/enigma/kelompok/repository/UserRepository.java
+++ src/main/java/com/enigma/kelompok/repository/UserRepository.java
@@ -5,11 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends
         JpaRepository<User, Integer>,
         JpaSpecificationExecutor<User> {
-    Optional<User> findByUsername(String username);
+    User findByUsername(String username);
 }
