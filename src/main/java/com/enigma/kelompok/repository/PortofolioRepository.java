958daade4abaf3a1b9d3abd4f72fd5d261a23e26
Alfin Ade Prasetya <alfin.prasetya24@gmail.com>
2024-07-06 07:47:49 +0700

    Fix: Repo annotation

--- src/main/java/com/enigma/kelompok/repository/PortofolioRepository.java
+++ src/main/java/com/enigma/kelompok/repository/PortofolioRepository.java
@@ -1,8 +1,11 @@
 package com.enigma.kelompok.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.enigma.kelompok.model.Portofolio;
+
+@Repository
 
 public interface PortofolioRepository extends JpaRepository<Portofolio, Integer> {
 
